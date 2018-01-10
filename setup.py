4b5ba852f92c6fd0bf8284fbd1fe8053772b61c9
sujan poudel <spoudel347@gmail.com>
2018-01-10 18:42:19 +0545

    configs are added

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyapk',
+    packages=find_packages(),
+    version='0.1',
+    description='A simple program to install apks from a directory to android phone via adb',
+    author='Sujan Poudel',
+    author_email='psuzzn@gmail.com',
+    url='https://github.com/psuzn/PyApk',
+    download_url='https://github.com/psuzn/PyApk',
+    keywords=['adb', 'apk', 'install', 'android'],  # arbitrary keywords
+    install_requires=[
+        'adb',
+        'os',
+        'sys',
+        'time',
+        'subprocess',
+        'itertools'
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Android Geeks',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'],
+    entry_points={
+        'console_scripts': [
+            'pyapk = pyapk.pyapk:pyapkInit'
+        ]},
+)
